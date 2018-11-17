dbc05814cf9a2074f5c56c1310f1f3ba934c8151
irocevo <irocevo@users.noreply.github.com>
2018-11-17 15:21:39 -0500

    change to opengapps

--- aicp_taimen.mk
+++ aicp_taimen.mk
@@ -23,5 +23,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/taimen/taimen:9/PQ1A.181105.017.A1/5081125:user/release-keys
 
+GAPPS_VARIANT := nano
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_FORCE_PIXEL_LAUNCHER := true
+
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
-$(call inherit-product-if-exists, vendor/googleapps/googleapps.mk)
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
